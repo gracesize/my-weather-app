8a608fa2ae4ba3972d023b6532d4ea83a7471868
gracesize <152830797+gracesize@users.noreply.github.com>
2024-01-01 12:34:07 -0500

    added weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement =document.querySelector("#description");
+  let humidityElement= document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+console.log(response.data.condition.description);
 
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+   timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+ descriptionElement.innerHTML = response.data.condition.description;
+ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+ windSpeedElement.innerHTML =`${Math.round(response.data.wind.speed)}km/h`;
+ temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sunday",
+"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+]
+let day = days[date.getDay()];
+if (minutes<10) {
+  minutes = `0${minutes}`;
+}
+return `${day} ${hours}:${minutes}`
 }
 
 function searchCity(city) {
@@ -31,4 +56,5 @@ function submitButton(event) {
 
   searchCity(searchInput.value);
 }
+
 searchCity("Miami");
